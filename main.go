6979b1b12c0b8afd3cee036152511bc03b8b3346
Ashley Conner <108750120+valardohaeris2@users.noreply.github.com>
2024-08-05 09:37:34 -0400

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 type book struct{
 	ID string `json:"id"`
 	Title string `json:"title"`
-	Author string `json:"author"`
+	Author string `json:"author"` 
 	Quantity int `json:"quantity"`
 }   
  

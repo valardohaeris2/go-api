0dcc36cec1bc5a25331717cc5808c892cffac06a
valardohaeris2 <aconner5674@gmail.com>
2024-10-17 16:05:59 -0400

    Add POST requests

--- main.go
+++ main.go
@@ -21,10 +21,24 @@ var books = []book{
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
+
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+	
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
 	router.Run("localhost:8080")
+	
 }
    
 
